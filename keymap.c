51234366f2e125ec1c2226a2bdabcb3ebf1714fd
André Silva <andre.beat@gmail.com>
2018-11-23 12:51:03 +0000

    switch right alt and gui keys and right symbol and ctrl keys

--- keymap.c
+++ keymap.c
@@ -80,9 +80,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                   // right hand
                                                                  ,KC_MINS ,KC_6 ,KC_7           ,KC_8   ,KC_9   ,KC_0             ,F(RSpec)
                                                                  ,KC_RBRC ,KC_Y ,KC_U           ,KC_I   ,KC_O   ,KC_P             ,F(RMdia)
-                                                                          ,KC_H ,LT(RBASE, KC_J),KC_K   ,KC_L   ,LT(RBASE,KC_SCLN),KC_RCTL
+                                                                          ,KC_H ,LT(RBASE, KC_J),KC_K   ,KC_L   ,LT(RBASE,KC_SCLN),F(RSymb)
                                                                  ,KC_TAB  ,KC_N ,KC_M           ,KC_COMM,KC_DOT ,KC_SLSH          ,KC_RSFT
-                                                                                ,KC_RGUI        ,KC_RALT,KC_HYPR,KC_MEH           ,F(RSymb)
+                                                                                ,KC_RALT        ,KC_RGUI,KC_HYPR,KC_MEH           ,KC_RCTL
                                                                  ,KC_LEFT ,KC_RGHT
                                                                  ,KC_UP
                                                                  ,KC_DOWN ,KC_ENT ,KC_SPC
