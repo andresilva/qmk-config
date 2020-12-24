12973e6e328e3dfa83f7f80d2098e04af63104ac
André Silva <andrerfosilva@gmail.com>
2020-12-24 17:29:26 +0000

    planck: fix includes

--- planck/rgb.h
+++ planck/rgb.h
@@ -1,4 +1,5 @@
 #include "lib/lib8tion/lib8tion.h"
+#include "layers.h"
 
 void rgb_matrix_layer_helper(uint8_t hue, uint8_t sat, uint8_t val, uint8_t mode, uint8_t speed, uint8_t led_type) {
     HSV hsv = {hue, sat, val};
